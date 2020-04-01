eeae93115b8a7352f619de747ec69b5a834ffcb6
samtalebi <samtalebi84@gmail.com>
2020-04-01 13:35:21 +0200

    some message

--- index.js
+++ index.js
@@ -1,15 +1,10 @@
 const http = require('http');
-const express = require('express');
-const app = express();
-const cors = require('cors');
+const app = require('/.app');
+const config = require('./utils/config');
+const logger = require('./utils/logger');
 
-const mongoUrl = 'mongodb://localhost/bloglist';
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+const server = http.createServer(app);
 
-app.use(cors());
-app.use(express.json());
-
-const PORT = 3003;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+server.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`);
 });
