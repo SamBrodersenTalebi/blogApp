f6299cbc1bf4b731997ad90e6fa195e6f4484c97
samtalebi <samtalebi84@gmail.com>
2020-04-10 20:10:54 +0200

    add userRouter to app.js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const cors = require('cors');
 const config = require('./utils/config');
 const blogRouter = require('./controllers/blogs');
+const userRouter = require('./controllers/users');
 const middleware = require('./utils/middleware');
 const logger = require('./utils/logger');
 const mongoose = require('mongoose');
@@ -12,12 +13,12 @@ logger.info('connecting to', config.MONGODB_URI);
 mongoose
   .connect(config.MONGODB_URI, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
   })
   .then(() => {
     logger.info('connected to MongoDB');
   })
-  .catch(error => {
+  .catch((error) => {
     logger.error('error connection to MongoDB:', error.message);
   });
 
@@ -26,6 +27,7 @@ app.use(express.json());
 app.use(middleware.requestLogger);
 
 app.use('/api/blogs', blogRouter);
+app.use('/api/users', userRouter);
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
