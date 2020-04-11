c699f313fe209c55a3a16748fc243feb477b2d73
samtalebi <samtalebi84@gmail.com>
2020-04-11 11:46:20 +0200

    user model

---
+++ controllers/users.js
@@ -0,0 +1,20 @@
+const bcrypt = require('bcrypt');
+const userRouter = require('express').Router();
+const User = require('../models/User');
+
+userRouter.post('/', async (req, res, next) => {
+  const body = req.body;
+
+  const saltRounds = 10;
+  const passwordHash = await bcrypt.hash(body.password, saltRounds);
+
+  const user = new User({
+    username: body.username,
+    name: body.name,
+    passwordHash,
+  });
+
+  const savedUser = await user.save();
+
+  res.json(savedUser);
+});
