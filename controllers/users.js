3e56bf788a8f19f8ffa5346af6db4b3cc213412f
samtalebi <samtalebi84@gmail.com>
2020-04-11 11:55:43 +0200

    error logger for password and username

--- controllers/users.js
+++ controllers/users.js
@@ -3,14 +3,22 @@ const userRouter = require('express').Router();
 const User = require('../models/User');
 
 userRouter.post('/', async (req, res, next) => {
-  const body = req.body;
+  const { password, name, username } = req.body;
 
+  if (!password || !username) {
+    return res.status(400).send('Password or username is missing');
+  }
+  if (password.length < 4 || username.length < 4) {
+    return res
+      .status(400)
+      .send('Password/username must be at least 3 characters long');
+  }
   const saltRounds = 10;
-  const passwordHash = await bcrypt.hash(body.password, saltRounds);
+  const passwordHash = await bcrypt.hash(password, saltRounds);
 
   const user = new User({
-    username: body.username,
-    name: body.name,
+    username: username,
+    name: name,
     passwordHash,
   });
 
