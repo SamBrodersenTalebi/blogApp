df72ab1a27fcc9ee76a764f03be7cf12530a4ad4
samtalebi <samtalebi84@gmail.com>
2020-04-11 14:38:55 +0200

    implement token-based authentication with login router

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const config = require('./utils/config');
+const loginRouter = require('./controllers/login');
 const blogRouter = require('./controllers/blogs');
 const userRouter = require('./controllers/users');
 const middleware = require('./utils/middleware');
@@ -27,6 +28,7 @@ app.use(cors());
 app.use(express.json());
 app.use(middleware.requestLogger);
 
+app.use('/api/login', loginRouter);
 app.use('/api/blogs', blogRouter);
 app.use('/api/users', userRouter);
 
